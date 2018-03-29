94ae2c4f47c405d44c108827641dd5f592cb3e73
pessonljxpd <903513623lj@gmail.com>
2018-03-29 23:51:55 +0800

    首页添加banner

---
+++ app/src/main/java/com/sharemall/sharemall/base/net/GlideImageLoader.java
@@ -0,0 +1,21 @@
+package com.sharemall.sharemall.base.net;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Banner图片加载器
+ *
+ * @author Shelly
+ * @date 2018/03/29
+ */
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+
+}
--- app/src/main/java/com/sharemall/sharemall/fragment/IndexFragment.java
+++ app/src/main/java/com/sharemall/sharemall/fragment/IndexFragment.java
@@ -2,6 +2,7 @@ package com.sharemall.sharemall.fragment;
 
 import android.os.Bundle;
 import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,10 +13,14 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.sharemall.sharemall.R;
 import com.sharemall.sharemall.adapter.MultipleItemQuickAdapter;
 import com.sharemall.sharemall.base.BaseLoadFragment;
+import com.sharemall.sharemall.base.net.GlideImageLoader;
 import com.sharemall.sharemall.beans.JasonResult;
 import com.sharemall.sharemall.beans.MultipleItem;
 import com.sharemall.sharemall.repository.DataServer;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +28,7 @@ import java.util.List;
  */
 public class IndexFragment extends BaseLoadFragment<JasonResult> {
 
+    private Banner mBanner;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -30,6 +36,7 @@ public class IndexFragment extends BaseLoadFragment<JasonResult> {
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index, container, false);
 
+        mBanner = view.findViewById(R.id.banner);
         mRecyclerView = view.findViewById(R.id.rv_list);
         final List<MultipleItem> data = DataServer.getMultipleItemData();
         final MultipleItemQuickAdapter multipleItemAdapter = new MultipleItemQuickAdapter(getContext(), data);
@@ -41,7 +48,11 @@ public class IndexFragment extends BaseLoadFragment<JasonResult> {
                 return data.get(position).getSpanSize();
             }
         });
+        multipleItemAdapter.addHeaderView(new ViewPager(getContext()));
+
         mRecyclerView.setAdapter(multipleItemAdapter);
+
+        setBanner();
 
         return view;
     }
@@ -52,6 +63,19 @@ public class IndexFragment extends BaseLoadFragment<JasonResult> {
         getLoaderManager().restartLoader(1, null, this);
     }
 
+    private void setBanner() {
+        List<String> imageUrl = new ArrayList<>();
+        imageUrl.add("http://img3.imgtn.bdimg.com/it/u=3823223463,3047520970&fm=23&gp=0.jpg");
+        imageUrl.add("http://img.hc360.com/auto-a/info/images/200803/8998419-10.jpg");
+        imageUrl.add("http://img.ichemo.cn/model/5188ebc96c74dd.jpg");
+        mBanner.setImageLoader(new GlideImageLoader())
+                .setImages(imageUrl)
+                .setDelayTime(3000)
+                .setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
+                .setIndicatorGravity(BannerConfig.RIGHT)
+                .start();
+    }
+
     @Override
     protected void loadFinished(int action, JasonResult arg1) {
     }
@@ -59,5 +83,17 @@ public class IndexFragment extends BaseLoadFragment<JasonResult> {
     @Override
     public Loader<JasonResult> onCreateLoader(int arg0, Bundle arg1) {
         return null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mBanner.startAutoPlay();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mBanner.stopAutoPlay();
     }
 }
